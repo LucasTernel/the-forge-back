95bac855f1d8d10848e46c8d7c563d24094b5ee8
Sladojevic Thomas <sladojevic.thomas@gmail.com>
2026-03-09 17:01:05 +0100

    add road of items and collection

--- app/Http/Controllers/api/CollectionController.php
+++ app/Http/Controllers/api/CollectionController.php
@@ -7,5 +7,20 @@ use Illuminate\Http\Request;
 
 class CollectionController extends Controller
 {
-    //
+    public function index()
+    {
+        $collections = \App\Models\Collection::with(['user', 'swords'])->get();
+        return response()->json($collections);
+    }
+
+    public function show($id)
+    {
+        $collection = \App\Models\Collection::with(['user', 'swords'])->find($id);
+
+        if (!$collection) {
+            return response()->json(['Mauvaise collection d épée, tranche mieux.'], 404);
+        }
+
+        return response()->json($collection);
+    }
 }
--- app/Http/Controllers/api/SwordController.php
+++ app/Http/Controllers/api/SwordController.php
@@ -7,5 +7,20 @@ use Illuminate\Http\Request;
 
 class SwordController extends Controller
 {
-    //
+    public function index()
+    {
+        $items = \App\Models\Sword::with(['type', 'origin', 'collection', 'media', 'criteria'])->get();
+        return response()->json($items);
+    }
+
+    public function show($id)
+    {
+        $item = \App\Models\Sword::with(['type', 'origin', 'collection', 'media', 'criteria'])->find($id);
+        
+        if (!$item) {
+            return response()->json(['Mauvaise épée, il serait temps de mieux trancher.'], 404);
+        }
+
+        return response()->json($item);
+    }
 }

aa5f615860521c239884ae6495b68fabee3f7047
Lucas Ternel <lucasternel@MacBook-Pro-de-Lucas.local>
2026-03-09 22:15:54 +0100

    add database seeder to check informations and fix /api/collections problems

--- app/Http/Controllers/api/CollectionController.php
+++ app/Http/Controllers/api/CollectionController.php
@@ -4,23 +4,24 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Collection;
 
 class CollectionController extends Controller
 {
     public function index()
     {
-        $collections = \App\Models\Collection::with(['user', 'swords'])->get();
-        return response()->json($collections);
+        $collections = Collection::with(['user', 'swords'])->get();
+        return response()->json($collections, 200, ['Content-Type' => 'application/json; charset=UTF-8'], JSON_UNESCAPED_UNICODE);
     }
 
     public function show($id)
     {
-        $collection = \App\Models\Collection::with(['user', 'swords'])->find($id);
+        $collection = Collection::with(['user', 'swords'])->find($id);
 
         if (!$collection) {
-            return response()->json(['Mauvaise collection d épée, tranche mieux.'], 404);
+            return response()->json(['Mauvaise collection d\'épée, tranche mieux.'], 404, ['Content-Type' => 'application/json; charset=UTF-8'], JSON_UNESCAPED_UNICODE);
         }
 
-        return response()->json($collection);
+        return response()->json($collection, 200, ['Content-Type' => 'application/json; charset=UTF-8'], JSON_UNESCAPED_UNICODE);
     }
 }

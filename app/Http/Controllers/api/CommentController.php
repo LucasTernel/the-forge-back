702ed801558520291c6f689b43a3178f6a6f9945
Conor McCracken <conormccracken1@icloud.com>
2026-03-10 14:29:12 +0100

    commentController fix

--- app/Http/Controllers/api/CommentController.php
+++ app/Http/Controllers/api/CommentController.php
@@ -20,7 +20,7 @@ class CommentController extends Controller
         $comments = Comment::where('sword_id', $swordId)->get();
         if ($comments->isEmpty())
             {
-            return response()->json(['message' => 'comments not found for sword'], 404, ['Content-Type' => 'application/json; charset=UTF-8'], JSON_UNESCAPED_UNICODE);
+            return response()->json(['message' => 'comments not found for sword'], 200, ['Content-Type' => 'application/json; charset=UTF-8'], JSON_UNESCAPED_UNICODE);
             }
 
             return response() -> json($comments,200,['Content-Type' => 'application/json; charset=UTF-8'], JSON_UNESCAPED_UNICODE);

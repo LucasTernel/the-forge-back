685b5b967a304bc0d9b46a6333adc1ef6dc38287
Lucas Ternel <lucasternel@example.com>
2026-03-10 14:18:19 +0100

    fix problems2

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\api\MediaController;
 use App\Http\Controllers\api\CriteriaController;
 use App\Http\Controllers\api\TypeController;
 use App\Http\Controllers\api\OriginController;
+<<<<<<< HEAD
+=======
+use App\Http\Controllers\api\MediaController;
+
+
+>>>>>>> 58d1ade (again)
 use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\FollowController;
 
@@ -34,6 +40,7 @@ Route::get('/collection/{id}', [CollectionController::class , 'show']);
 Route::get('/collections', [CollectionController::class , 'index']);
 Route::get('/collections/{id}', [CollectionController::class , 'show']);
 
+<<<<<<< HEAD
 Route::get('/origins', [OriginController::class , 'index']);
 Route::get('/origins/{id}', [OriginController::class , 'show']);
 
@@ -41,10 +48,24 @@ Route::get('/criterias', [CriteriaController::class , 'index']);
 
 Route::post('/media', [MediaController::class, 'store'])->middleware('auth:sanctum');
 
+=======
+<<<<<<< HEAD
+>>>>>>> 58d1ade (again)
 
 
 Route::get('/types', [TypeController::class , 'index']);
 Route::get('/types/{id}', [TypeController::class , 'show']);
+<<<<<<< HEAD
 
 Route::get('/users/{id}/followers', [FollowController::class, 'getFollowers']);
 Route::get('/users/{id}/following', [FollowController::class, 'getFollowing']);
+=======
+=======
+Route::get('/origins', [OriginController::class , 'index']);
+Route::get('/origins/{id}', [OriginController::class , 'show']);
+
+Route::get('/criterias', [CriteriaController::class , 'index']);
+
+Route::post('/media', [MediaController::class, 'store'])->middleware('auth:sanctum');
+>>>>>>> 90cfade (add road of items and collection v2)
+>>>>>>> 58d1ade (again)

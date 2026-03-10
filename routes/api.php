7d7245d77a17e9c755587f6a2da354e89bb8b4b5
Lucas Ternel <lucasternel@MacBook-Pro-de-Lucas.local>
2026-03-10 09:40:26 +0100

    modification of roads

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
+use App\Http\Controllers\api\SwordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\CollectionController;
+use App\Http\Controllers\api\CriteriaController;
+use App\Http\Controllers\api\TypeController;
+use App\Http\Controllers\api\OriginController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/items', [SwordController::class , 'index']);
-Route::get('/items/{id}', [SwordController::class , 'show']);
+Route::get('items', [SwordController::class , 'index']);
+Route::get('items/{id}', [SwordController::class , 'show']);
 
 Route::get('/collection/{id}', [CollectionController::class , 'show']);
 Route::get('/collections', [CollectionController::class , 'index']);

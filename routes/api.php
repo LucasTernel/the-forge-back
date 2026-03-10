ef6a823f16ab993066607c5e8c64adcdb1b69b8b
Sladojevic Thomas <sladojevic.thomas@gmail.com>
2026-03-10 14:08:15 +0100

    add road of social relations

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
             return $request->user();
         }
+        
         );
     });
 
